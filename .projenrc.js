0dd53698a6dc95c5471fa9feb2cc277d75c3a410
Eli Polonsky <epolon@amazon.com>
2022-05-26 14:09:26 +0300

    publish

--- .projenrc.js
+++ .projenrc.js
@@ -24,8 +24,6 @@ const project = new cdk.JsiiProject({
   },
 });
 
-project.addFields({ private: true });
-
 for (const example of ['rds-db-instance']) {
   const exampleDir = `examples/${example}`;
   const synth = project.addTask(`synth:${example}`);
